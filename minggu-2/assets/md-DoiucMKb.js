08973b6466fa706380210b24e8a46f05681e90ea
HarisHerdiansyah <95732447+HarisHerdiansyah@users.noreply.github.com>
2026-08-30 12:36:27 +0000

    Deploying to gh-pages from @ SammyFZeb/Slide-Praktikum-PBO-2026@c964784fbf528c1d90f0c6c7ff87cdc3841956d4 🚀

---
+++ minggu-2/assets/md-DoiucMKb.js
@@ -0,0 +1,60 @@
+import{L as e,T as t,Y as n,_ as r,b as i,g as a,gt as o,mt as s}from"./modules/shiki-DeTF74yl.js";import{it as c,rt as l}from"./index-B6cS0odr.js";import{t as u}from"./slidev/default-7TOQ1N1o.js";var d={__name:`24_Latihan_Soal_2.md__slidev_24`,setup(d){let{$slidev:f,$nav:p,$clicksContext:m,$clicks:h,$page:g,$renderContext:_,$frontmatter:v}=c();return m.setup(),(c,d)=>(e(),r(u,o(t(s(l)(s(v),23))),{default:n(()=>[...d[0]||=[a(`h1`,null,`Soal 2`,-1),a(`p`,null,`Tambahkan fitur agar pemilik toko bisa melihat semua produk dan customer bisa membeli produk.`,-1),a(`div`,{class:`grid grid-cols-[1fr_0.8fr] gap-8 items-start mt-4`},[a(`div`,null,[a(`p`,{class:`text-base`},[a(`ul`,null,[a(`li`,null,`Untuk menampilkan semua produk, cukup menampilkan semua produk yang telah ditambahkan admin.`),a(`li`,null,[i(`Transaksi dimulai dari memasukkan `),a(`code`,null,`customer_id`),i(`, verifikasi (mencocokkan `),a(`code`,null,`customer_id`),i(` inputan dengan yang sudah dibuat di Main), memasukkan `),a(`code`,null,`nama_produk`),i(` dan kuantitas produk yang mau dibeli, dan menampilkan subtotalnya.`)])])])]),a(`div`,{class:`text-xs bg-gray-900 border border-gray-700 rounded-lg p-4 font-mono leading-relaxed h-90 overflow-y-auto`},[a(`div`,{class:`text-gray-400 mb-2 border-b border-gray-700 pb-1 uppercase tracking-wider text-[10px]`},`Contoh Output`),a(`pre`,null,[a(`code`,null,`//Input: 2
+=== SISTEM MANAJEMEN TOKO ===
+1. Tambahkan Produk
+2. Tampilkan semua produk 
+3. Transaksi
+
+Pilih menu : 2
+
+================================
+=== LIST BARANG===
+================================
+Nama: baju fardan
+Kategori: barang bekas
+Harga:  17500
+Stok Awal: 1
+—----------------------------------------
+Nama: ayam kabir
+Kategori: makanan
+Harga:  15000
+Stok Awal: 99
+—----------------------------------------
+Nama: ijazah jokowi
+Kategori: mitos
+Harga:  500
+Stok Awal: 1
+—----------------------------------------
+
+
+//Input: 3
+=== SISTEM MANAJEMEN TOKO ===
+1. Tambahkan Produk
+2. Tampilkan semua produk 
+3. Transaksi
+
+Pilih menu : 3
+
+================================
+=== Login User===
+Masukkan id : 1
+
+—--------------------------------------------
+=== Login Success===
+—--------------------------------------------
+
+=== LIST BARANG===
+Nama: baju fardan
+Harga:  17500
+—----------------------------------------
+Nama: ayam kabir
+Harga:  15000
+—----------------------------------------
+Nama: ijazah jokowi
+Harga:  500
+—-----------------------------------------
+
+Nama barang yang ingin dibeli : baju fardan
+Nama: baju fardan
+Jumlah : 1
+Subtotal:  17500
+`)])])],-1)]]),_:1},16))}};export{d as default};
---
+++ minggu-2/assets/md-DyGejkMR.js
@@ -0,0 +1,13 @@
+import{L as e,T as t,Y as n,_ as r,b as i,g as a,gt as o,mt as s}from"./modules/shiki-DeTF74yl.js";import{it as c,rt as l}from"./index-B6cS0odr.js";import{t as u}from"./slidev/default-7TOQ1N1o.js";var d={__name:`23_Latihan_Soal_1.md__slidev_23`,setup(d){let{$slidev:f,$nav:p,$clicksContext:m,$clicks:h,$page:g,$renderContext:_,$frontmatter:v}=c();return m.setup(),(c,d)=>(e(),r(u,o(t(s(l)(s(v),22))),{default:n(()=>[...d[0]||=[a(`h1`,null,`Soal 1`,-1),a(`p`,null,`Buat program CLI berbasis menu interaktif:`,-1),a(`div`,{class:`grid grid-cols-[1fr_0.8fr] gap-8 items-start mt-4`},[a(`div`,null,[a(`p`,{class:`text-base`},[a(`ol`,null,[a(`li`,null,[a(`strong`,null,[i(`Buat class entitas (`),a(`code`,null,`Product`),i(` dan `),a(`code`,null,`Customer`),i(`).`)])])]),a(`ul`,null,[a(`li`,null,[i(`Class `),a(`code`,null,`Product`),i(` menyimpan: `),a(`code`,null,`nama_produk`),i(`, `),a(`code`,null,`kategori`),i(`, `),a(`code`,null,`harga`),i(`, `),a(`code`,null,`stok_awal`),i(` dan method `),a(`code`,null,`hitung_subtotal()`),i(`.`)]),a(`li`,null,[i(`Class `),a(`code`,null,`Customer`),i(` menyimpan: `),a(`code`,null,`customer_id`),i(`, `),a(`code`,null,`nama_customer`),i(`.`)])]),a(`ol`,{start:`2`},[a(`li`,null,[a(`strong`,null,`Objek Customer:`),i(` Buat objek `),a(`code`,null,`Customer`),i(` di method Main. ID dan nama dibebaskan.`)]),a(`li`,null,[a(`strong`,null,`Mekanisme Tambah Produk:`),i(` Buatlah mekanisme menambahkan produk, dimana semua atribut di kelas Produk harus didapatkan dari input.`)])])])]),a(`div`,{class:`text-xs bg-gray-900 border border-gray-700 rounded-lg p-4 font-mono leading-relaxed`},[a(`div`,{class:`text-gray-400 mb-2 border-b border-gray-700 pb-1 uppercase tracking-wider text-[10px]`},`Contoh Output (Input Menu: 1)`),a(`pre`,null,[a(`code`,null,`=== SISTEM MANAJEMEN TOKO ===
+1. Tambahkan Produk
+2. Tampilkan semua produk 
+3. Transaksi
+
+Pilih menu : 1
+
+================================
+Nama: baju fardan
+Kategori: barang bekas
+Harga:  17500
+Stok Awal: 1
+`)])])],-1)]]),_:1},16))}};export{d as default};
